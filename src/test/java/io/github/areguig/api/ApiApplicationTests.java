53dad737b841245c551446ebcabbe8e8679699d7
Akli Reguig <areguig@users.noreply.github.com>
2020-01-15 13:20:09 +0100

    Create maven.yml

--- src/test/java/io/github/areguig/api/ApiApplicationTests.java
+++ src/test/java/io/github/areguig/api/ApiApplicationTests.java
@@ -2,8 +2,11 @@ package io.github.areguig.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("it")
 class ApiApplicationTests {
 
 	@Test
